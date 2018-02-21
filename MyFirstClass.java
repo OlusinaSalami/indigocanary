5cfde8331f1ddb4c7ab12c312b1288251882bbfb
OlusinaSalami <36657132+OlusinaSalami@users.noreply.github.com>
2018-02-21 06:58:09 +0000

    Adding my First Java file to git via UI

---
+++ MyFirstClass.java
@@ -0,0 +1,24 @@
+package Day1.examples;
+
+public class MyFirstClass {
+/*
+ * This is my first Java Program 
+ */
+	public static void main(String[] args) {
+		// My first statement 
+		System.out.println("Hello World");
+		
+		int x = 10;
+		int y = 20;
+		int result = x+y;
+		System.out.println("Result = "+ result);
+		
+		int a = 10;
+		int b = 3;
+		int answer = a % b;
+		System.out.println("The Remainder is "+ answer);
+		
+		
+	}
+
+}
